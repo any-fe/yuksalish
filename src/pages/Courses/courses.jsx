26a3815a38f665f23f739c2d4a7745ac7b3f8b05
Sardor <sardorggs77@gmail.com>
2026-03-20 20:01:14 +0500

    first commit

--- src/pages/Courses/courses.jsx
+++ src/pages/Courses/courses.jsx
@@ -241,6 +241,7 @@ export default function Courses() {
                 </div>
             </section>
 
+            
             {/* ══ FOOTER ══ */}
             <footer className="footer">
                 <div className="wrap">
